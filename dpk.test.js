451fe439da908154c64ca05c57c19681a681275a
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2023-02-09 15:03:55 +0100

    test: update regex, test edge cases

--- dpk.test.js
+++ dpk.test.js
@@ -13,17 +13,18 @@ describe("deterministicPartitionKey", () => {
     // hex encoding is 2 chars for 1 byte so expected string length is 2*64 = 128
     expect(hash).toHaveLength(128);
     // check we do have a hex string
-    expect(hash).toMatch(/^[0-9a-fA-F]+$/);
+    // who doesn't love regex
+    expect(hash).toMatch(/^(0x|0h)?[0-9A-F]+$/i);
   });
   it("Returns a sha3-512 hash when given an input that is not a string and not flagged as a partition key", () => {
     const key = { badKey: 998 };
     const hash = deterministicPartitionKey(key);
     expect(typeof hash).toBe("string");
     expect(hash).toHaveLength(128);
-    expect(hash).toMatch(/^[0-9a-fA-F]+$/);
+    expect(hash).toMatch(/^(0x|0h)?[0-9A-F]+$/i);
   });
   it("Returns a partition key unchanged when given a partition key string less than 257 chars", () => {
-    const key = "donald_duck";
+    const key = "1".repeat(256);
     const resultingKey = deterministicPartitionKey({ partitionKey: key });
     expect(resultingKey).toBe(key);
   });
@@ -33,17 +34,17 @@ describe("deterministicPartitionKey", () => {
     expect(resultingKey).toBe(JSON.stringify(key));
   });
   it("Returns the sha3-512 hash of a partition key when it is a string over 256 chars", () => {
-    const key = "donald_duck".repeat(100);
+    const key = "1".repeat(257);
     const hash = deterministicPartitionKey({ partitionKey: key });
     expect(typeof hash).toBe("string");
     expect(hash).toHaveLength(128);
-    expect(hash).toMatch(/^[0-9a-fA-F]+$/);
+    expect(hash).toMatch(/^(0x|0h)?[0-9A-F]+$/i);
   });
   it("Returns the sha3-512 hash of a partition key when it is not a string but whose string representation is over 256 chars", () => {
     const key = Array.from({ length: 500 }, (val, i) => i);    
     const hash = deterministicPartitionKey({ partitionKey: key });
     expect(typeof hash).toBe("string");
     expect(hash).toHaveLength(128);
-    expect(hash).toMatch(/^[0-9a-fA-F]+$/);
+    expect(hash).toMatch(/^(0x|0h)?[0-9A-F]+$/i);
   });
 });
